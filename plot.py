bfe08b17c67e0d11873412555f015de51fd17148
Gosia <gosia@gosia>
2020-03-30 00:44:14 +0200

    wyswietlanie krzywych

--- plot.py
+++ plot.py
@@ -1,53 +1,46 @@
+from classcurve import *
 import matplotlib.pyplot as plt
 from matplotlib.collections import EventCollection
 import numpy as np
 
 
-xs = np.array([])
-ys = np.array([])
-x = np.array([])
-y = np.array([])
-xs = np.append(xs, [x])
-ys = np.append(ys, [y])
+curves = []
 i = 0  # indeks aktualnej krzywej
 n = 0  # liczba krzywych
 
 fig, ax = plt.subplots()
 ax.set(xlim=(0, 100), ylim=(0, 100))
-ax.plot(x, y)
+ax.plot([], [])
 
 # poprawić wyświetlanie wszystkich dotychczasowych kryzwych no i kolorki ogarnąć
 
 
-def add_point(event):
-    global x, xs, i
-    global y, ys
-    x = np.append(x, [event.xdata])
-    y = np.append(y, [event.ydata])
-    xs[i] = x
-    ys[i] = y
-    ax.plot(x, y, ".")
-    ax.plot(x, y)
+def add_point_to(event):
+    global curves, i
+    c = curves[i]
+    c.add_point(event.xdata, event.ydata)
+    curves[i] = c
+    show_curves()
+
+
+def show_curves():
+    global curves
+    for c in curves:
+        ax.plot(c.x, c.y, color=c.color)
+    for c in curves:
+        if c.control_points:
+            ax.plot(c.x, c.y, ".", color=c.color)
     plt.show()
 
 
-def shiftv(vx, vy):
-    global x, y, xs, ys, i
-    x = x+vx
-    y = y+vy
-
-
 def new_curve():
-    global xs, ys, x, y, i, n
-    x = np.array([])
-    y = np.array([])
-    xs = np.append(xs, [x])
-    ys = np.append(ys, [y])
+    global curves, i, n
+    c = Curve()
+    curves.append(c)
     i = n
     n += 1
 
 
-cid = fig.canvas.mpl_connect('button_press_event', add_point)
+new_curve()
+cid = fig.canvas.mpl_connect('button_press_event', add_point_to)
 plt.show()
-
-print(x[99])
