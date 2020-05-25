9070f994f89914a6da77fddc499240951cb2cc1b
Gosia <gosia@gosia>
2020-05-26 01:52:50 +0200

    point description, mause events

--- menu_functions.py
+++ menu_functions.py
@@ -13,23 +13,34 @@ gi.require_version('Gtk', '3.0')
 
 curves = []
 i = 0  # indeks aktualnej krzywej
+ip = 0  # indeks aktualnego punktu
 n = 0  # liczba krzywych
 activtext = "1"
 activx = "0"
 activy = "0"
 
-actcurindlabel = Gtk.Label(i)
+actcurindlabel = Gtk.Label(i+1)
+actpointindlabel = Gtk.Label(ip+1)
+
 
 fig, ax = plt.subplots()
 ax.set(xlim=(0, 100), ylim=(0, 100))
 ax.plot([], [])
 canvas = FigureCanvas(fig)  # a Gtk.DrawingArea
 
+# obsluga myszki - podlaczenia
+#canv_add_point = canvas.mpl_connect('button_press_event', add_point_to)
+canv_add_point = None
+canv_motion = None
+canv_release = None
+press = 0
+
 
 def show_curves():
-    global curves, i
+    global curves, i, ip
     plt.cla()
     ax.set(xlim=(0, 100), ylim=(0, 100))
+    nc = 0
     for c in curves:
         if c.visibility == 1:
             if c.type == 1:
@@ -41,14 +52,12 @@ def show_curves():
                             linewidth=c.thick, color=c.color)
             if c.type == 3:
                 if c.i > 1:
-                    #t1 = np.arange(0.0, 100.0, 0.1)
+                    # t1 = np.arange(0.0, 100.0, 0.1)
                     # ax.plot(Bezier(t1, 0, 100, c.x, c.i), Bezier(t1, 0, 100, c.y, c.i), label=i,
                     #       linewidth=c.thick, color=c.color)
                     t1 = np.arange(c.x[0], c.x[c.i-1], 0.1)
                     ax.plot(Bezier(t1, c.x[0], c.x[c.i-1], c.x, c.i), Bezier(t1, c.x[0], c.x[c.i-1], c.y, c.i), label=i,
                             linewidth=c.thick, color=c.color)
-                # ax.legend()
-                # plt.gca().add_artist(plt.legend())
             if c.type == 4:
                 if c.i > i:
                     # print("here")
@@ -64,9 +73,35 @@ def show_curves():
                                 linewidth=c.thick, color=c.color)
                         # ax.plot(t1, t1,
                         #       linewidth=c.thick, color=c.color)
+            if c.type == 5:
+                if c.i > i:
+                    # print("here")
+                    M = deterMnofs3(c.x, c.y, c.i)
+                    # print("here")
+                    for k in range(1, c.i):
+                        # print(k)
+                        mx = max(c.x[k-1], c.x[k])
+                        mn = min(c.x[k-1], c.x[k])
+                        hk = mx-mn
+                        t1 = np.arange(mn, mx, hk/100)
+                        ax.plot(t1, OISF3(t1, c.x[k], c.y[k], c.x[k-1], c.y[k-1], M[k], M[k-1]), label=i,
+                                linewidth=c.thick, color=c.color)
+                        # ax.plot(t1, t1,
+                        #       linewidth=c.thick, color=c.color)
+            if c.i > 1:
+                ax.annotate(str(nc+1),
+                            ((9*c.x[0]+c.x[1])/10, (9*c.y[0]+c.y[1])/10), color="red")
     for c in curves:
         if c.control_points:
             ax.plot(c.x, c.y, ".", color=c.color)
+            if curves[i] == c:
+                if c.color != 'red':
+                    # print("here")
+                    ax.plot(c.x[ip], c.y[ip], ".", color='red')
+                else:
+                    ax.plot(c.x[ip], c.y[ip], color='blue')
+            for j in range(1, c.i+1):
+                ax.annotate(j, (c.x[j-1], c.y[j-1]))
     canvas.draw_idle()
 
 
@@ -79,8 +114,75 @@ def add_point_to(event):
         show_curves()
 
 
+def change_point(event):
+    global curves, n, i, ip
+    cp = np.array((event.xdata, event.ydata))
+    dist = 10000
+    probpoint = ip
+    probcurve = i
+    for j in range(0, n):
+        c = curves[j]
+        for k in range(0, c.i):
+            po = np.array((c.x[k], c.y[k]))
+            curdist = np.linalg.norm(po-cp)
+            if curdist < dist and curdist < 1:
+                dist = curdist
+                probpoint = k
+                probcurve = j
+    i = probcurve
+    ip = probpoint
+    actcurindlabel.set_text(str(i+1))
+    actpointindlabel.set_text(str(ip+1))
+    show_curves()
+
+
+def moving_point(event):
+    global press
+    press = 1
+
+
+def on_motion(event):
+    global press, curves, i, ip
+    if press == 1:
+        c = curves[i]
+        c.x[ip] = event.xdata
+        c.y[ip] = event.ydata
+        show_curves()
+
+
+def on_release(event):
+    global press
+    press = 0
+    show_curves()
+
+
+def move_point(event):
+    global canv_add_point, canvas
+    canvas.mpl_disconnect(canv_add_point)
+    #canv_choose_point = canvas.mpl_connect('button_press_event', change_point)
+    canv_add_point = canvas.mpl_connect('button_press_event', moving_point)
+    canv_motion = canvas.mpl_connect('motion_notify_event', on_motion)
+    canv_release = canvas.mpl_connect('button_release_event', on_release)
+
+
+def choose_point(event):
+    global canv_add_point, canvas
+    canvas.mpl_disconnect(canv_add_point)
+    #canv_choose_point = canvas.mpl_connect('button_press_event', change_point)
+    canv_add_point = canvas.mpl_connect('button_press_event', change_point)
+
+
+def add_point(event):
+    global canv_add_point
+    canvas.mpl_disconnect(canv_add_point)
+    #canv_choose_point = canvas.mpl_connect('button_press_event', change_point)
+    canv_add_point = canvas.mpl_connect('button_press_event', add_point_to)
+
+
 def new_curve(event):
-    global curves, i, n
+    global curves, i, n, canvas, canv_add_point
+    canvas.mpl_disconnect(canv_add_point)
+    canv_add_point = canvas.mpl_connect('button_press_event', add_point_to)
     c = Curve()
     curves.append(c)
     i = n
@@ -104,7 +206,7 @@ def change_activy(widget, event):
 
 
 def change_color_of(event):
-    global curves, i, activtext
+    global curves, i, activtext, canvas
     c = curves[i]
     c.change_color(activtext)
     show_curves()
@@ -142,15 +244,15 @@ def change_activ_curve(event):
     k = int(activtext)
     if k < n:
         i = k
-    actcurindlabel.set_text(str(i))
+    actcurindlabel.set_text(str(i+1))
 
 
 def del_point(event):
-    global curves, activtext, i
+    global curves, activtext, i, ip
     c = curves[i]
-    pind = int(activtext)
-    if pind < c.i:
-        c.remove_point(pind)
+    c.remove_point(ip)
+    if ip > 0:
+        ip = ip-1
     show_curves()
 
 
@@ -166,16 +268,18 @@ def shift_curve(event):
 def swap_p(event):
     global curves, activx, activy, i
     c = curves[i]
-    k = int(activx)
-    l = int(activy)
+    k = int(activx)-1
+    l = int(activy)-1
     if k < c.i and l < c.i:
         c.swap_points(k, l)
     show_curves()
 
 
 def undo(event):
-    global curves, i
+    global curves, i, ip
     c = curves[i]
+    if ip == c.i-1:
+        ip = 0
     c.remove_point(c.i-1)
     show_curves()
 
@@ -189,10 +293,11 @@ def change_type(event):
 
 def division_curve_point(event):
     global curves, i, n, activtext
-    print("here")
+    # print("here")
     c = curves[i]
     if c.type == 3:
         t = int(activtext)
+        # if t>=c.x[0] and t<=c.x[n-1]:
         X = c.x
         Y = c.y
         np = c.i
@@ -202,6 +307,7 @@ def division_curve_point(event):
         d.i = c.i
         d.type = c.type
         d.color = c.color
+        d.thick = c.thick
         d.x, d.y = deCasteljauright(t, X[0], X[np-1], X, Y, np)
         n += 1
         curves.append(d)
